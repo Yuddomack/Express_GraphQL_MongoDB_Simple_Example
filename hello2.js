c0825660e646146faa0cde1841a2983ed7c318cc
Yuddomack <darr880406@gmail.com>
2018-10-14 19:59:59 +0900

    Hello World of GraphQLSchema

--- hello2.js
+++ hello2.js
@@ -19,7 +19,9 @@ const queryType = new Graphql.GraphQLObjectType({
     },
     persons: {
       type: new Graphql.GraphQLList(personType),
-      resolve: () => {
+      resolve: (post, args, context, info) => {
+        console.log(context);
+        
         return [
           {name:"kim", age: 20},
           {name:"lee", age: 30},
@@ -33,8 +35,11 @@ const queryType = new Graphql.GraphQLObjectType({
 const schema = new Graphql.GraphQLSchema({query: queryType});
 
 var app = express();
+const session = {id: "1001", expires: 20000};
+
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true,
+  context: session,
 }));
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
