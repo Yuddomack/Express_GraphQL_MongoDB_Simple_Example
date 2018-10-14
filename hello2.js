89d52a9b422fc524a0401339c6b3090dd0645223
Yuddomack <darr880406@gmail.com>
2018-10-14 20:40:04 +0900

    Hello World of GraphQLSchema

--- hello2.js
+++ hello2.js
@@ -19,14 +19,26 @@ const queryType = new Graphql.GraphQLObjectType({
     },
     persons: {
       type: new Graphql.GraphQLList(personType),
+      args: {
+        name: {type: Graphql.GraphQLString},
+        age: {type: Graphql.GraphQLInt}
+      },
       resolve: (post, args, context, info) => {
         console.log(context);
-        
+        console.log(args);
+        const {name, age} = args;
+
         return [
           {name:"kim", age: 20},
           {name:"lee", age: 30},
           {name:"park", age: 40},
-        ];
+        ].filter((person) => {
+          if(!name && !age){ return true; }
+          if(!age && name && person.name === name){ return true; }
+          if(!name && age && person.age === age){ return true; }
+          if(name && age && person.name === name && person.age === age){ return true; }
+          return false;
+        });
       }
     }
   }
