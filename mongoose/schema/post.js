987d67258b5c52c6935c88d5ba1089a84d83de68
Yuddomack <darr880406@gmail.com>
2018-10-18 01:02:47 +0900

    init schema

--- mongoose/schema/post.js
+++ mongoose/schema/post.js
@@ -1,8 +1,8 @@
-export.modules = function(mongoose){
+module.exports = function(mongoose){
   return new mongoose.Schema({
     title: String,
     content: String,
-    author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'} // 다른 스키마를 쓸 수 있게 하려면?
+    author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}, // 다른 스키마를 쓸 수 있게 하려면?
     c_date: { type: Date, default: Date.now }
   });
 };
--- mongoose/schema/user.js
+++ mongoose/schema/user.js
@@ -1,7 +1,7 @@
-export.modules = function(mongoose){
+module.exports = function(mongoose){
   return new mongoose.Schema({
     email: String,
     pwd: String,
     c_date: { type: Date, default: Date.now }
   });
-});
+};
