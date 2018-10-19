849973e33fa651a5051f773773271690e4897235
Yuddomack <darr880406@gmail.com>
2018-10-19 14:50:21 +0900

    add app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var app = express();
+
+const {schema, resolver: root} = require('./graphql/schema');
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
